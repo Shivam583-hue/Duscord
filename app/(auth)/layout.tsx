ae57cdd288167e3af40ba690a13b80434292b4ab
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-17 06:08:03 +0600

    clerk auth added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+import React from 'react'
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="">
+    <div className='flex flex-col items-center justify-center h-screen'>
       {children}
     </div>
-  );
-};
+  )
+}
+
+export default AuthLayout
